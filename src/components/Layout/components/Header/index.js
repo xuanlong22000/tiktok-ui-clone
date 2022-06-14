e9d8c9245b62457eaa0fe7a84c47f92b559c5edb
Xuan Long <xuanlong22000@gmail.com>
2022-06-14 17:00:53 +0700

    Header #2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,36 @@
 import React from 'react'
+import images from '~/assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import classNames from 'classnames/bind'
 import styles from './Header.module.scss'
 
 const cx = classNames.bind(styles)
 
 export const Header = () => {
+
     return (
         <>
             <header className={cx('wrapper')}>
                 <div className={cx('inner')}>
 
+                    <div className={cx('logo')}>
+                        <img src={images.logo} alt='tik tok' />
+                    </div>
+
+                    <div className={cx('search')}>
+                        <input placeholder='Search accounts and videos' spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                    <div className={cx('actions')}></div>
                 </div>
             </header>
         </>
